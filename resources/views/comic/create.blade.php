2a838a35975457e2fd64aa14f514572a666e84b8
ariannadepasquale <depasqualearianna@gmail.com>
2022-10-05 18:26:08 +0200

    add  old value in create.blade

--- resources/views/comic/create.blade.php
+++ resources/views/comic/create.blade.php
@@ -8,34 +8,34 @@
         @csrf
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control" id="title" name="title"/>
+            <input type="text" class="form-control" id="title" name="title" value="{{old('title')}}"/>
         </div>
         
         <div class="mb-3">
             <label for="price" class="form-label">Price</label>
-            <input type="price" class="form-control" id="price" name="price"/>
+            <input type="price" class="form-control" id="price" name="price" value={{old('price')}}/>
         </div>
         
         <div class="mb-3">
             <label for="series" class="form-label">Series</label>
-            <input type="series" class="form-control" id="series" name="series"/>
+            <input type="series" class="form-control" id="series" name="series"value={{old('series')}}/>
         </div>
         
         <div class="mb-3">
             <label for="thumb" class="form-label">Thumb</label>
-            <input type="thumb" class="form-control" id="thumb" name="thumb"/>
+            <input type="thumb" class="form-control" id="thumb" name="thumb" value={{old('thumb')}}/>
         </div>
         
         <div class="mb-3">
             <label for="sale_date" class="form-label">Sale Date</label>
-            <input type="sale_date" class="form-control" id="sale_date" name="sale_date"/>
+            <input type="sale_date" class="form-control" id="sale_date" name="sale_date" value={{old('sale_date')}}/>
         </div>
         
         <div class="mb-3">
             <label for="type" class="form-label">Type</label>
             <select name="type" id="type" class="form-select">
-            <option value="comic_book">Comic Book</option>
-            <option value="graphic_novel">Graphic Novel</option>
+            <option value={{(old('type')=='comic_book')?'selected':''}}>Comic Book</option>
+            <option value={{(old('type')=='graphic_novel')?'selected':''}}>Graphic Novel</option>
             
         </select>
         </div>
